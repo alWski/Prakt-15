4370145a12c1ce8b970859d8d163f6c04ddf42fa
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-19 01:30:42 +0700

    Add files via upload

---
+++ ex19.py
@@ -0,0 +1,20 @@
+def count(a, b):
+    if a < b:
+        a, b = b, a
+    
+    if b == 0:
+        return 0
+    
+    sq_count = a // b
+    
+    remaining = count(b, a % b)
+    
+    return sq_count + remaining
+    
+def main():
+    a = int(input())
+    b = int(input())
+    print(count(a, b))
+
+if __name__ == "__main__":
+    main()
