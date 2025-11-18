8bd44c1c6855db3d1c8107d7217732363708b080
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-18 23:50:38 +0700

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,16 +1,17 @@
-def ind_max(a, index=0, max_index=0):
+def ind_maxlist(a, index=0, max_index=0):
     if index == len(a):
         return max_index
     
     if a[index] > a[max_index]:
         max_index = index
     
-    return ind_max(a, index + 1, max_index)
+    return ind_maxlist(a, index + 1, max_index)
     
 def main():
     n = int(input())
     a = [int(input()) for i in range(n)]
-    print(ind_max(a))
+    print(ind_maxlist(a))
 
 if __name__ == "__main__":
     main()
+
