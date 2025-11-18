ea3f26ca55a4b3198a01192b9abc79742e407722
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-18 23:52:39 +0700

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,18 @@
+def odd_list(a, n):
+    if n == 0:
+        return []
+    
+    result = odd_list(a, n-1)
+    
+    if a[n-1] % 2 == 0:
+        result.append(a[n-1])
+    
+    return result
+    
+def main():
+    n = int(input())
+    a = [int(input()) for i in range(n)]
+    print(odd_list(a, n))
+
+if __name__ == "__main__":
+    main()
