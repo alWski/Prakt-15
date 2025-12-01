f688b9c152a11b6de95eac0d2016fc881bde605b
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-12-01 11:32:15 +0700

    Create ex16.py

---
+++ ex16.py
@@ -0,0 +1,15 @@
+def ten_to_ran(x, n):
+    if n // x == 0:
+        return str(n%x)
+    if n % x > 9:
+    	s = 'nnnnnnnnnnABCDEF'
+    	return ten_to_ran(x, n // x) + str(s[n % x])
+    return ten_to_ran(x, n // x) + str(n % x)
+    
+def main():
+	x = int(input())
+	n = int(input())
+	print(ten_to_ran(x, n))
+
+if __name__ == "__main__":
+    main()
